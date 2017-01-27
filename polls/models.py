4ba610ab2c0c3e2fbac231db381d21b858843922
RayWangChunlei <wangchl2.ray@gmail.com>
2017-01-27 10:54:55 +0800

    Update models.py

--- polls/models.py
+++ polls/models.py
@@ -1,7 +1,5 @@
-import datetime
-
 from __future__ import unicode_literals
-
+import datetime
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils import timezone
