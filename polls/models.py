cb528504d5a7d763b698bb614653bceaf4af4ea1
RayWangChunlei <wangchl2.ray@gmail.com>
2017-01-27 10:50:48 +0800

    Update models.py

--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
+from django.utils import timezone
 
 # Create your models here.
 @python_2_unicode_compatible  # only if you need to support Python 2
