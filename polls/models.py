bc7c6afe2596b1c6915e67fa246f2d6145fba135
RayWangChunlei <wangchl2.ray@gmail.com>
2017-01-29 21:40:38 +0800

    Update models.py

--- polls/models.py
+++ polls/models.py
@@ -12,7 +12,11 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+#    def was_published_recently(self):
+#        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 @python_2_unicode_compatible  # only if you need to support Python 2
 class Choice(models.Model):
